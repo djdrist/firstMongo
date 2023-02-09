9c6c004ff69f360adfa5662dabee40de0c88530c
Lukasz Kallas <lukasz.kallas@gmail.com>
2023-02-09 11:48:31 +0100

    Add tests

---
+++ models/test/employee.test.js
@@ -0,0 +1,70 @@
+const Employee = require('../employee.model.js');
+const mongoose = require('mongoose');
+const expect = require('chai').expect;
+
+describe('Employee', () => {
+	it('should throw an error if no "firstName" arg', () => {
+		const dep = new Employee({ lastName: 'Doe', department: 'IT' });
+
+		dep.validate((err) => {
+			expect(err.errors.firstName).to.exist;
+		});
+	});
+	it('should throw an error if no "lastName" arg', () => {
+		const dep = new Employee({ firtName: 'Jane', department: 'IT' });
+
+		dep.validate((err) => {
+			expect(err.errors.lastName).to.exist;
+		});
+	});
+	it('should throw an error if no "department" arg', () => {
+		const dep = new Employee({ firstName: 'Jane', lastName: 'Doe' });
+
+		dep.validate((err) => {
+			expect(err.errors.department).to.exist;
+		});
+	});
+
+	it('should throw an error if "firstName" is not a string', () => {
+		const cases = [{}, []];
+		for (let firstName of cases) {
+			const dep = new Employee({ firstName, lastName: 'Doe', department: 'IT' });
+
+			dep.validate((err) => {
+				expect(err.errors.firstName).to.exist;
+			});
+		}
+	});
+	it('should throw an error if "lastName" is not a string', () => {
+		const cases = [{}, []];
+		for (let lastName of cases) {
+			const dep = new Employee({ lastName, firtName: 'Jane', department: 'IT' });
+
+			dep.validate((err) => {
+				expect(err.errors.lastName).to.exist;
+			});
+		}
+	});
+	it('should throw an error if "department" is not a string', () => {
+		const cases = [{}, []];
+		for (let department of cases) {
+			const dep = new Employee({ firstName: 'Jane', lastName: 'Doe', department });
+
+			dep.validate((err) => {
+				expect(err.errors.department).to.exist;
+			});
+		}
+	});
+
+	it('should not throw an error if all data is okay', () => {
+		const dep = new Employee({ firstName: 'Jane', lastName: 'Doe', department: 'IT' });
+
+		dep.validate((err) => {
+			expect(err).to.not.exist;
+		});
+	});
+
+	after(() => {
+		mongoose.models = {};
+	});
+});
